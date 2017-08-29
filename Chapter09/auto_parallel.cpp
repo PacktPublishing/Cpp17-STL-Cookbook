293f58f122f3a00b32001f14e9c72aaa0fdd547c
Jacek Galowicz <jacek@galowicz.de>
2017-08-29 09:02:17 +0200

    Fix 09/auto_parallel.cpp
    - Fix execution policy header file name in include.
    - Add comment about the algorithm itself.

--- Chapter09/auto_parallel.cpp
+++ Chapter09/auto_parallel.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <algorithm>
 #include <algorith>
-#include <execution_policy>
+#include <execution>
 
 using namespace std;
 
@@ -11,6 +11,12 @@ static bool odd(int n) { return n % 2; }
 
 int main()
 {
+    // Note that, as the recipe in the book says, this algorithm
+    // is not supposed to be a solution of some problem.
+    // This example program is rather about demonstrating how to
+    // use the new execution policies in order to use the new
+    // parallel versions of existing STL algorithms.
+
     vector<int> d (500);
 
     mt19937 gen;
